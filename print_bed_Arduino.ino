55c93fe3ea712e428e79d9bfefcbe4dd93f44c2a
carl <carlinchan@yahoo.com.hk>
2021-04-25 12:44:18 +0800

    first commit

---
+++ print_bed_Arduino.ino
@@ -0,0 +1,171 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Name: Chan Car Lin
+// UID: 3035604568
+// Course: ELEC3442 Embedded System
+// Project: Smart print bed for 3d printer
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Creating a BluetoothSerial object
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include "BluetoothSerial.h"
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Creating a TaskHandle_t objects for multi-core programming (Bluetooth communication)
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+TaskHandle_t task_bluetooth;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Variables for servo motors
+// The index refer to the corresponding servo motor: {top_left, top_right, botton_left, botton right}
+//
+// top_left                         top_right
+//        --------------------------
+//        |                        |
+//        |                        |
+//        |                        |
+//        |                        |
+//        |       Print bed        |
+//        |                        |
+//        |                        |
+//        |                        |
+//        --------------------------
+// bottom_left                      bottom_right
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int pin_list[4] = {27, 14, 12, 13};   // Servo pins
+
+#include <ESP32Servo.h>
+Servo servo_0;
+Servo servo_1;
+Servo servo_2;
+Servo servo_3;
+Servo servo_list[4] = {servo_0, servo_1, servo_2, servo_3};   // Servo objects
+
+int angle_list[4] = {0, 0, 0, 0};   // Used to store the servo current angle
+int msg_angle[4] = {0, 0, 0, 0};    // Used to store the adjust angle sent from Raspberry Pi
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Variables for servos control
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+String msg = "";    // Incoming message
+String command = "";    // This command used to adjust all servo motors
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Variables for core checking
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+bool core_check_main = false;
+bool core_check_bt = false;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Used to adjust all servo
+// Para: command_string - The command string
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void servo_adjust() {
+
+  Serial.println("command: " + command);
+  
+  int first_comma = command.indexOf(',');
+  int second_comma = command.indexOf(',', first_comma+1);
+  int third_comma = command.indexOf(',', second_comma+1);
+
+  msg_angle[0] = (command.substring(0, first_comma)).toInt();
+  msg_angle[1] = (command.substring(first_comma+1, second_comma)).toInt();
+  msg_angle[2] = (command.substring(second_comma+1, third_comma)).toInt();
+  msg_angle[3] = (command.substring(third_comma+1)).toInt();    
+
+  for (int i = 0; i < 4; i++) {
+    angle_list[i] = angle_list[i] + msg_angle[i];
+    if (angle_list[i] < 0) {
+      angle_list[i] = 0;
+    }
+    else if (angle_list[i] > 180) {
+      angle_list[i] = 180;
+    }
+    servo_list[i].write(angle_list[i]);
+  }
+
+  command = "";
+  
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// The task function for bluetooth communication core
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void task_bluetooth_code(void * pvParameters) {
+  for(;;) {
+    // For checking the bluetooth run on which core
+    // Execute once on the initial
+    if (core_check_bt == false) {
+      Serial.println();
+      Serial.println("Bluetooth core: " + String(xPortGetCoreID()) + ", with time: " + millis());
+      core_check_bt = true;
+    }  
+    // Listening the BT serial
+    if (SerialBT.available()) {
+      char temp = SerialBT.read();      
+      msg += temp; 
+    }
+    else {
+      // Checking the msg whether is emtpy or not
+      if (msg.length() > 0) {
+        command = msg;
+      }
+      msg = "";
+    }
+    delay(10);
+  }  
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// setup()
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin("ESP32"); //Bluetooth device name
+
+  // Initialize the setting of second core
+  // Because the default running core of ESP32 is core 1, so the bluetooth task should be assigned to core 0
+  xTaskCreatePinnedToCore(
+    task_bluetooth_code,  // Task function
+    "task_bluetooth",  // Task name
+    10000,  // Stack size of task
+    NULL,  // Parameter of task
+    0, // Priority of task - 0: lower; 1: higher (same as setup() and loop()) 
+    &task_bluetooth,  // Task handle
+    0   // Core where the task should run
+    );
+
+  for (int i = 0; i < 4; i++) {
+    servo_list[i].attach(pin_list[i]);
+    angle_list[i] = 90;
+    servo_list[i].write(angle_list[i]);
+  }
+  
+  Serial.println("Ready!");
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// loop()
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void loop() {
+  
+  // For checking the main loop() run on which core
+  // Execute once on the initial
+  if (core_check_main == false) {
+    Serial.println();
+    Serial.println("Main core: " + String(xPortGetCoreID()) + ", with time: " + millis());
+    core_check_main = true;  
+  }
+  
+  // Adjust the servos with a specific command
+  if (command != "") {
+    servo_adjust();
+  } 
+
+}
---
+++ print_bed_Pi.py
@@ -0,0 +1,111 @@
+############################################################
+# Name: Chan Car Lin
+# UID: 3035604568
+# Course: ELEC3442 Embedded System
+# Project: Smart print bed for 3d printer
+############################################################
+
+
+############################################################
+# Initializing the variables
+############################################################
+import bluetooth
+from sense_hat import *
+from time import sleep
+
+# The upper limit value
+upper_limit = 330
+# The lower limit value
+lower_limit = 30
+# The list used to store the adjusted value
+msg_int = [0] * 4
+# The interval of the while loop (unit: second)
+interval = 2
+
+# Bluetooth socker object
+sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+# Port number
+port = 1
+# MAC address of ESP32
+serverMACAddress = '7C:9E:BD:F4:61:26'
+# Connecting to the ESP32 via Bluetooth
+sock.connect((serverMACAddress, port))
+
+# Initializing the senseHat
+sense = SenseHat()
+sense.clear()
+
+
+############################################################
+# Main loop
+############################################################
+while True:
+    # --------------------------------------------------
+    # Getting the orientation of the senseHat
+    # --------------------------------------------------
+    orientation = sense.get_orientation()
+    p = round(orientation["pitch"], 0)
+    r = round(orientation["roll"], 0)
+    # print("p: %s, r: %s" % (p,r))   
+    # y = round(orientation["yaw"], 0)
+    # print("p: %s, r: %s, y: %s" % (p,r,y))
+
+    # --------------------------------------------------
+    # Calculating the value of the adjustment (Pitch)
+    # For testing, the pitch value is bounded by
+    # pitch <= lower_limit and pitch >= upper_limit
+    # --------------------------------------------------
+    if (p <= lower_limit or p >= upper_limit):
+        if (p <= lower_limit):
+            p_angle = round(p/2)
+            msg_int[0] += p_angle
+            msg_int[1] -= p_angle
+            msg_int[2] += p_angle          
+            msg_int[3] -= p_angle
+        else:
+            p_angle = round((360-p)/2)
+            msg_int[0] -= p_angle
+            msg_int[1] += p_angle
+            msg_int[2] -= p_angle           
+            msg_int[3] += p_angle
+
+    # msg_string = str(msg_int[0]) + "," + str(msg_int[1]) + "," + str(msg_int[2]) + "," + str(msg_int[3]);
+    # print(msg_string)
+
+    # --------------------------------------------------
+    # Calculating the value of the adjustment (Roll)
+    # For testing, the roll value is bounded by
+    # roll <= lower_limit and roll >= upper_limit
+    # --------------------------------------------------
+    if (r <= lower_limit or r >= upper_limit):
+        if (r <= lower_limit):
+            r_angle = round(r/2)
+            msg_int[0] -= r_angle
+            msg_int[1] -= r_angle
+            msg_int[2] += r_angle
+            msg_int[3] += r_angle           
+        else:
+            r_angle = round((360-r)/2)
+            msg_int[0] += r_angle
+            msg_int[1] += r_angle
+            msg_int[2] -= r_angle
+            msg_int[3] -= r_angle            
+
+    msg_string = str(msg_int[0]) + "," + str(msg_int[1]) + "," + str(msg_int[2]) + "," + str(msg_int[3]);
+    print(msg_string)
+
+    # --------------------------------------------------
+    # Sending out the value,
+    # then clear the previous values
+    # --------------------------------------------------
+    sock.send(msg_string)
+    msg_int[0] = 0
+    msg_int[1] = 0
+    msg_int[2] = 0
+    msg_int[3] = 0
+    
+    sleep(interval)
+
+
+
+
